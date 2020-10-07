cc90826de5ee926f8c5a6c21786167b31d353060
austin <55255361+PirateTurner1@users.noreply.github.com>
2020-10-07 03:22:57 -0600

    finding problems, and fixing it.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// giving the top h1-h2 header.
+document.querySelector('header > h1').innerText = "The Nonsense Story";
+document.querySelector('header > h2').innerText = "String Manipulation";
+
+//giving the body an h1-h2 header.
+document.getElementById('head1').innerHTML = "The Tail of The Tortoise And The Hair.";
+document.getElementById('head2').innerHTML = "What I have learned is:";
